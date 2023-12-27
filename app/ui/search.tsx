2e44228179b122951f2b8453454b8daa4bf31e17
Naoki Haba <naosprintrunner6385@gmail.com>
2023-12-27 21:47:48 +0900

    Remove unused import in search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,6 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useDebouncedCallback } from "use-debounce"
-import { param } from "ts-interface-checker"
 
 export default function Search({placeholder}: { placeholder: string }) {
     // React Router内でクエリパラメーター（URLの ?query=param 形式）を取得・変更するためのフック
